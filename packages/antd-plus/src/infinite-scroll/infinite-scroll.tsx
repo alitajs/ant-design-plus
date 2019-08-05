58b6cc542ac9ccb4d2c0bef48da8662c14f32bfe
wangxingkang <wangxingkang@sensoro.com>
2019-08-05 23:42:52 +0800

    feat: add FitText component

--- packages/antd-plus/src/infinite-scroll/infinite-scroll.tsx
+++ packages/antd-plus/src/infinite-scroll/infinite-scroll.tsx
@@ -3,7 +3,15 @@ import { Spin } from 'antd';
 import classNames from 'classnames';
 
 export interface IInfiniteScrollProps {
-
+  prefixCls?: string;
+  className?: string;
+  style?: React.CSSProperties;
+  loader?: React.ReactNode;
+  offset?: number;
+  loadMore?: () => void;
+  onScroll?: () => void;
+  // 是否
+  initialLoad?: boolean;
 }
 
 export interface IState {
@@ -11,9 +19,20 @@ export interface IState {
 }
 
 class InfiniteScroll extends React.Component<IInfiniteScrollProps, IState> {
+  static defaultProps = {
+    prefixCls: 'ant-plus-infinite-scroll',
+    offset: 20
+  };
+
   render() {
+    const { className, prefixCls } = this.props;
+
     return (
-      <div className={classNames()}>
+      <div
+        className={classNames(className, {
+          [`${prefixCls}`]: true
+        })}
+      >
 
       </div>
     )
