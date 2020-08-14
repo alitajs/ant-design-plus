691cf9868a0782b4ca9e08b26a323fda781a2060
wangxingkang <156148958@qq.com>
2020-08-14 11:51:49 +0800

    feat(ellipsis): 功能完善

--- src/ellipsis/ellipsis.tsx
+++ src/ellipsis/ellipsis.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { Tooltip } from 'antd';
 import isStyleSupport from 'antd/es/_util/styleChecker';
-import { TooltipProps } from 'antd/es/tooltip';
-import isBoolean from 'lodash/isBoolean';
+import ResizeObserver from 'resize-observer-polyfill';
 import { ConfigContext } from '../config-provider';
 import EllipsisText from './ellipsis-text';
 import { TooltipType } from './interface';
@@ -25,7 +24,7 @@ interface EllipsisProps {
   /**
    * 最大显示的行数，超出则截取省略
    */
-  lines?: string;
+  lines?: number;
   /**
    * 显示的字符的最大长度，超过则截取省略
    */
@@ -57,11 +56,45 @@ const Ellipsis: React.FC<EllipsisProps> = ({
   const handleRef = React.useRef<HTMLSpanElement>(null);
   const shadowRef = React.useRef<HTMLDivElement>(null);
   const shadowChildrenRef = React.useRef<HTMLDivElement>(null);
+  const widthNodeRef = React.useRef<HTMLSpanElement>(null);
+  const lineClampNodeRef = React.useRef<HTMLDivElement>(null);
+  const resizeObserver = React.useRef<any>(null);
 
   const [text, setText] = React.useState<string>('');
   const [targetCount, setTargetCount] = React.useState<number>(0);
+  const [isEllipsisActive, setIsEllipsisActive] = React.useState<boolean>(false);
 
   const { getPrefixCls } = React.useContext(ConfigContext);
+
+  React.useEffect(() => {
+    if (handleRef.current) {
+      computeLine();
+    }
+    if (width || lines) {
+      let target;
+      if (width) {
+        target = widthNodeRef.current;
+      } else if (lines && isLineClampSupport) {
+        target = lineClampNodeRef.current;
+      } else {
+        return;
+      }
+      detectEllipsisActive(target);
+
+      resizeObserver.current = new ResizeObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.target === target) {
+            detectEllipsisActive(target);
+          }
+        });
+      });
+      resizeObserver.current.observe(target);
+    }
+
+    return () => {
+      resizeObserver.current && resizeObserver.current.disconnect();
+    };
+  }, []);
 
   const prefixCls = getPrefixCls('ellipsis');
   const cls = classNames(`${prefixCls}-ellipsis`, className, {
@@ -69,7 +102,105 @@ const Ellipsis: React.FC<EllipsisProps> = ({
     [`${prefixCls}-line`]: lines && !isLineClampSupport,
     [`${prefixCls}-lineClamp`]: lines && isLineClampSupport
   });
+  const tooltipProps = getTooltipProps(tooltip);
 
+  const detectEllipsisActive = (node: HTMLElement) => {
+    setIsEllipsisActive(
+      node.offsetHeight < node.scrollHeight || node.offsetWidth < node.scrollWidth
+    );
+  };
+
+  /**
+   *
+   *
+   */
+  const computeLine = () => {
+    if (lines && !isLineClampSupport) {
+      const text = shadowChildrenRef.current.innerText || shadowChildrenRef.current.textContent;
+      const lineHeight = parseInt(getComputedStyle(rootRef.current).lineHeight, 10);
+      const targetHeight = lines * lineHeight;
+      contentRef.current.style.height = `${targetHeight}px`;
+      const totalHeight = shadowChildrenRef.current.offsetHeight;
+      const shadowNode = shadowRef.current.firstChild;
+
+      if (totalHeight <= targetHeight) {
+        setText(text);
+        setTargetCount(text.length);
+        return;
+      }
+
+      // bisection
+      const len = text.length;
+      const mid = Math.ceil(len / 2);
+
+      const count = bisection(targetHeight, mid, 0, len, text, shadowNode);
+
+      setText(text);
+      setTargetCount(count);
+    }
+  };
+
+  const bisection = (th, m, b, e, text, shadowNode) => {
+    const suffix = '...';
+    let mid = m;
+    let end = e;
+    let begin = b;
+    shadowNode.innerHTML = text.substring(0, mid) + suffix;
+    let sh = shadowNode.offsetHeight;
+
+    if (sh <= th) {
+      shadowNode.innerHTML = text.substring(0, mid + 1) + suffix;
+      sh = shadowNode.offsetHeight;
+      if (sh > th || mid === begin) {
+        return mid;
+      }
+      begin = mid;
+      if (end - begin === 1) {
+        mid = 1 + begin;
+      } else {
+        mid = Math.floor((end - begin) / 2) + begin;
+      }
+      return bisection(th, mid, begin, end, text, shadowNode);
+    }
+    if (mid - 1 < 0) {
+      return mid;
+    }
+    shadowNode.innerHTML = text.substring(0, mid - 1) + suffix;
+    sh = shadowNode.offsetHeight;
+    if (sh <= th) {
+      return mid - 1;
+    }
+    end = mid;
+    mid = Math.floor((end - begin) / 2) + begin;
+    return bisection(th, mid, begin, end, text, shadowNode);
+  };
+
+  // 一种限制都没有返回原值
+  if (!lines && !length && !width) {
+    return <span className={cls}>{children}</span>;
+  }
+
+  // 宽度限制
+  if (width) {
+    const node = (
+      <span ref={widthNodeRef} className={cls} style={{ ...style, maxWidth: width }}>
+        {children}
+      </span>
+    );
+    return tooltipProps ? (
+      <Tooltip
+        {...tooltipProps}
+        overlayClassName={`${prefixCls}-tooltip`}
+        title={isEllipsisActive ? children : null}
+      >
+        {node}
+      </Tooltip>
+    ) : (
+      node
+    );
+  }
+
+  // 字数限制
   if (length) {
     return (
       <EllipsisText
@@ -78,6 +209,32 @@ const Ellipsis: React.FC<EllipsisProps> = ({
         text={(children || '') as string}
         fullWidthRecognition={fullWidthRecognition}
       />
+    );
+  }
+
+  // 行数限制
+  const id = `ant-plus-ellipsis-${`${new Date().getTime()}${Math.floor(Math.random() * 100)}`}`;
+
+  if (isLineClampSupport) {
+    const style = `#${id}{-webkit-line-clamp:${lines};-webkit-box-orient: vertical;}`;
+
+    const node = (
+      <div ref={lineClampNodeRef} id={id} className={cls}>
+        <style>{style}</style>
+        {children}
+      </div>
+    );
+
+    return tooltipProps ? (
+      <Tooltip
+        {...tooltipProps}
+        overlayClassName={`${prefixCls}-tooltip`}
+        title={isEllipsisActive ? children : null}
+      >
+        {node}
+      </Tooltip>
+    ) : (
+      node
     );
   }
 
