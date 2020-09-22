646e10be4a12bd5a95ac8687654d15545c217110
wangxingkang <156148958@qq.com>
2020-09-22 14:19:04 +0800

    feat: export DaysRange

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ export { default as ButtonList } from './button-list';
 export { default as ErrorBoundary } from './error-boundary';
 export { default as ScrollableBar } from './scrollable-bar';
 export { default as Authorized } from './authorized';
+export { default as DaysRange } from './days-range';
 
 // 数据展示
 export { default as Tags } from './tags';
