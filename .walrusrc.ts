1520a365072b73907e59ea14f8c0cdea1bb7db41
wangxingkang <156148958@qq.com>
2020-10-29 18:15:02 +0800

    chore(deps): update deps

--- .walrusrc.ts
+++ .walrusrc.ts
@@ -2,7 +2,7 @@ import { Config } from '@walrus/cli';
 
 const config: Config = {
   entry: {
-    ignore: ['.umi', 'common', 'components', 'hooks', 'interface', 'theme', 'style', 'locale']
+    ignore: ['.umi', '.umi-production','common', 'components', 'hooks', 'interface', 'theme', 'style', 'locale']
   }
 };
 
