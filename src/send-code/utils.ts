d4a37f500df0e46534aefbe0fb1c54e408f82b76
wangxingkang <156148958@qq.com>
2020-11-25 23:35:46 +0800

    fix(send-code): 修复报错
    Close #69

--- src/send-code/utils.ts
+++ src/send-code/utils.ts
@@ -1,7 +1,7 @@
 // 获取格式化的模板文本
 export function getTemplateText(runText: string, second: number): string {
   if (runText?.indexOf('{%s}') !== -1) {
-    return runText.replace(/\{([^{]*?)%s(.*?)\}/g, second.toString());
+    return runText.replace(/\{([^{]*?)%s(.*?)\}/g, second?.toString());
   } else {
     return runText;
   }
